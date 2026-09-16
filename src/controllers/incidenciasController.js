c06855cfdd35b4ab15fd68d332d6e64ad92f5a90
dsvillalobos <00177525@uca.edu.sv>
2026-09-16 16:03:17 -0600

    fix: agregar validaciones

--- src/controllers/incidenciasController.js
+++ src/controllers/incidenciasController.js
@@ -5,6 +5,7 @@ const incidencias = [
     area: "Contabilidad",
     descripcion: "Problema con la impresora",
     prioridad: "Alta",
+    estado: "Pendiente",
   },
   {
     id: 2,
@@ -12,11 +13,12 @@ const incidencias = [
     area: "Recursos Humanos",
     descripcion: "Problema con el sistema de nómina",
     prioridad: "Alta",
+    estado: "Pendiente",
   },
 ];
 
 export const getIncidencias = function (req, res) {
-  res.json(incidencias);
+  res.status(200).json(incidencias);
 };
 
 export const getIncidenciaById = function (req, res) {
@@ -24,23 +26,34 @@ export const getIncidenciaById = function (req, res) {
 
   for (let i = 0; i < incidencias.length; i++) {
     if (incidencias[i].id === parseInt(id)) {
-      res.json(incidencias[i]);
+      res.status(200).json(incidencias[i]);
       return;
     }
   }
 
-  res.json({ message: "Incidencia no encontrada" });
+  res.status(404).json({ message: "Incidencia no encontrada" });
 };
 
 export const createIncidencia = function (req, res) {
   const { empleado, area, descripcion, prioridad } = req.body;
 
+  // Validaciones
+  if (!empleado || !area || !descripcion || !prioridad) {
+    return res.status(400).json({ message: "Debes ingresar todos los datos" });
+  }
+
+  if (prioridad !== "Alta" && prioridad !== "Media" && prioridad !== "Baja") {
+    return res.status(400).json({ message: "Prioridad no valida" });
+  }
+
+  // Si pasa las validaciones, entonces hay que registrar la incidencia
   const nuevaIncidencia = {
     id: incidencias.length + 1,
     empleado: empleado,
     area: area,
     descripcion: descripcion,
     prioridad: prioridad,
+    estado: "Pendiente", // Por defecto, el estado inicial es Pendiente
   };
 
   incidencias.push(nuevaIncidencia);
@@ -52,7 +65,7 @@ export const delIncidenciaById = function (req, res) {
   const id = parseInt(req.params.id);
 
   // findIndex recibe una función comparadora directa
-  const index = incidencias.findIndex(item => item.id === id);
+  const index = incidencias.findIndex((item) => item.id === id);
 
   // Si no existe, findIndex retorna -1
   if (index === -1) {
@@ -75,12 +88,13 @@ export const clasificacionIncidencia = function (req, res) {
   }
 
   // Se busca el objeto
-  const incidenciaEncontrada = incidencias.find(item => item.id === parseInt(id));
+  const incidenciaEncontrada = incidencias.find(
+    (item) => item.id === parseInt(id),
+  );
 
   if (!incidenciaEncontrada) {
     return res.status(404).json({ message: "Incidencia no encontrada" });
   }
-
 
   switch (incidenciaEncontrada.prioridad) {
     case "Alta":
